f0170e53967df6490deec9e45e4cb5d85e9d1117
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-07-08 14:51:04 +0200

    :zap: Add prop type to onChangeInput

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./Input.css";
-const Input = ({ className, type, disabled, ...props }) => (
+const Input = ({ className, type, disabled, onChangeInput, ...props }) => (
   <input
     type={type}
     className={`input
         ${disabled && ".disabled"}
         ${className}`}
     disabled={disabled}
+    onClick={onChangeInput}
     {...props}
   />
 );
@@ -16,13 +17,15 @@ const Input = ({ className, type, disabled, ...props }) => (
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  onChangeInput: PropTypes.func
 };
 
 Input.defaultProps = {
   className: "",
   type: "text",
-  disabled: false
+  disabled: false,
+  onChangeInput: () => {}
 };
 
 export default Input;
