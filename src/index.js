6f807fac5d7f7de79d85c26c1aea3ce530de4b66
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-07-08 15:47:37 +0200

    :zap: wrap app with react router
    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import Container from "./components/Container";
+import List from "./containers/List";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-ReactDOM.render(<h1>Subreddit Feeds</h1>,
-  document.getElementById('root')
+const App = () => (
+  <Container>
+    <Switch>
+      <Route exact path="/" component={List} />
+    </Switch>
+  </Container>
 );
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
