08337256dcf0389de896503ffbc56947d9aa7327
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-07-09 05:40:52 +0200

    :lipstick: Add spinner loader component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Spinner.css";
+
+const Spinner = ({ ...props }) => (
+  <svg
+    className="spinner"
+    viewBox="0 0 66 66"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <circle
+      className="path"
+      fill="none"
+      strokeWidth="6"
+      strokeLinecap="round"
+      cx="33"
+      cy="33"
+      r="30"
+    />
+  </svg>
+);
+
+export default Spinner;
