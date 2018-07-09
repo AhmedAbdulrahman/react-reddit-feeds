c82d6d16ec709a88dcdb62a4518363754d4e4816
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-07-09 05:38:15 +0200

    :recycle: Refactor api fn all cases

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,37 @@
-// Helper functions
+const REDDIT = "https://www.reddit.com";
 
 // format Subreddit score
 export const formatScore = num =>
   num > 999 ? (num / 1000).toFixed(1) + "k" : num;
 
-const parse = response =>
-  new Promise(resolve => {
-    response
-      .json()
-      .then(data => resolve(data))
-      .catch(() => resolve());
-  });
+// Returns a list of posts from a subredd.
+export const getSubreddits = async (
+  subreddit = "",
+  subredditID = "",
+  limit = 25,
+  offset = "all"
+) => {
+  let url;
+  console.log(subreddit);
+  switch (offset) {
+    case "next":
+      url = `${REDDIT}/r/${subreddit}.json?limit=${limit}&after=${subredditID}`;
+      break;
+    case "prev":
+      url = `${REDDIT}/r/${subreddit}.json?limit=${limit}&before=${subredditID}`;
+      break;
+    default:
+      url = `${REDDIT}/r/${subreddit}.json?limit=${limit}`;
+      break;
+  }
+  try {
+    const res = await fetch(url);
+    const result = await res.json();
+    const { after, children } = result.data;
+    return { after, posts: children };
+  } catch (exception) {
+    console.error(`Failed to retrieve subreddit information: (${exception})`);
+  }
+};
 
-// Helper on top of fetch.
-export const request = (url, options) =>
-  fetch(`https://www.reddit.com/r/${url}`, {
-    ...options
-  }).then(parse);
+// Returns single post from subreddit.
