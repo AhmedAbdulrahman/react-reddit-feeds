58903a197cc20d4255521c0a0b4310bbfa8a9b89
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-07-09 07:21:55 +0200

    :rocket: Link SinglePost component to react router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Container from "./components/Container";
 import List from "./containers/List";
+import Post from "./containers/Post";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
@@ -11,6 +12,7 @@ const App = () => (
   <Container>
     <Switch>
       <Route exact path="/" component={List} />
+      <Route path="/r/:sub/comments/:author/:title" component={Post} />
     </Switch>
   </Container>
 );
